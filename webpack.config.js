2ddb4d61ae4c0d3a85a48d94d0145274bc406e79
Danil Kaist <kaystd@gmail.com>
2020-04-18 13:53:50 +0300

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,18 @@ const webpack = require('webpack')
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
