c4c9b71e297119fc7ec6f4997cd638b86ee06bda
Danil Kaist <kaystd@gmail.com>
2020-04-19 14:33:08 +0300

    Add Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
 import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Container from '@material-ui/core/Container'
+import { createMuiTheme, createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/core/styles'
 
-export const App = (): React.ReactElement => <div>Hello World!</div>
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      backgroundColor: theme.palette.background.paper,
+      height: '100vh'
+    },
+  }),
+)
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#673ab7',
+    },
+    secondary: {
+      main: '#ef6c00'
+    },
+  },
+})
+
+export const App = (): React.ReactElement => {
+  const classes = useStyles()
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth='lg' className={classes.container}>
+      </Container>
+    </ThemeProvider>
+  )
+}
